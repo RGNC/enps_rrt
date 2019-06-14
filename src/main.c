9fa869fa42ec936a254f81dac17c2e7a9daedb22
Ignacio Perez <perezh@us.es>
2019-06-14 12:12:02 +0200

    user interface

--- src/main.c
+++ src/main.c
@@ -16,40 +16,70 @@
 
 int main(int argc, char* argv[])
 {
-	int selection=4;	
+	int selection=1;
+	int algorithm = RRT_ALGORITHM;
+	int seed = time(NULL);
+	
+	if (argc==1) {
+		printf("\nFormat: %s [m] [a] [s]\n",argv[0]);
+		printf("Where: \n");
+		printf("\n[m] is the map index\n");
+		printf("\t1 = %s\n",MAP1);
+		printf("\t2 = %s\n",MAP2);
+		printf("\t3 = %s\n",MAP3);
+		printf("\t4 = %s\n",MAP4);
+		printf("\n[a] is the algorithm index\n");
+		printf("\t1 = RRT ALGORITHM\n");
+		printf("\t2 = RRT STAR ALGORITHM\n");
+		printf("\n[s] is the random seed\n");
+		return 0;
+	}
+		
 	if (argc>1) {
 		selection = atoi(argv[1]);	
 	}
-
 	
-	srand(time(NULL));
+	if (argc>2) {
+		algorithm = atoi(argv[2])-1;
+	}
+
+	if (argc>3) {
+		seed = atoi(argv[3]);
+	}
+
+	if (algorithm!= RRT_ALGORITHM && algorithm != RRT_STAR_ALGORITHM) {
+		printf("Invalid algorithm\n");
+		return 0;
+	}
 	
 	RRT_PARAMS params;
 	RRT_VARS vars;
-	
-	
+		
 	switch (selection)
 	{
 		case 1:
-			init_params(MAP1,12,0.15,DEBUG,RRT_STAR_ALGORITHM,&params);
+			init_params(MAP1,12,0.15,DEBUG,algorithm,&params);
 			init_vars(8,10,&params,&vars);
 		break;
 		case 2:
-			init_params(MAP2,12,0.15,DEBUG,RRT_STAR_ALGORITHM,&params);
+			init_params(MAP2,12,0.15,DEBUG,algorithm,&params);
 			init_vars(32,9.3,&params,&vars);
 		break;
 		case 3:
-			init_params(MAP3,12,0.15,DEBUG,RRT_STAR_ALGORITHM,&params);
+			init_params(MAP3,12,0.15,DEBUG,algorithm,&params);
 			init_vars(21.5,21.5,&params,&vars);
 		break;
 		case 4:
-			init_params(MAP4,12,0.15,DEBUG,RRT_STAR_ALGORITHM,&params);
+			init_params(MAP4,12,0.15,DEBUG,algorithm,&params);
 			init_vars(5.25,30.45,&params,&vars);
 		break;
 		default:
 			printf("Invalid map\n");
 			return 0;
-	}	
+	}
+	
+	srand(seed);
+		
 	while (!vars.halt) {
 		enps_rrt_one_iteration(&params,&vars);
 	}
