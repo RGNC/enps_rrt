7d99e61a034d1359d10a9fdfba4dbcf792730ad0
Miguel Angel Martinez del Amor <mdelamor@us.es>
2019-07-22 12:24:17 +0200

    working version with CUB

--- src/enps_rrt_gpu.cu
+++ src/enps_rrt_gpu.cu
@@ -18,12 +18,16 @@ using namespace cub;
 
 struct _dev_pointers {
 	float *da,*db;
-	float *dx,*dy,*dd,*ddp;
-	float *dcubtemp,*dm;
-	cub::KeyValuePair<int, int> *d_argmin;
+	float *dx,*dy,*dd,*ddp,*dm;
+	void *dcubtemp;
+	size_t temp_bytes;
+	int num_multiproc;
+	cub::KeyValuePair<int,float> *d_argmin;
 } devp;
 
 #define MAX(a,b) (a>b?a:b)
+#define MIN(a,b) (a<b?a:b)
+#define THRES_GPU 500
 
 void init_params(const char* file, int n, float delta, int debug, int algorithm, RRT_PARAMS* params)
 {
@@ -87,8 +91,12 @@ void init_params(const char* file, int n, float delta, int debug, int algorithm,
 	
 	params->device = -1;
   	cudaGetDevice(&params->device);
+	struct cudaDeviceProp devprop;
+	cudaGetDeviceProperties(&devprop,params->device);
+ 	printf("Using GPU: %s\n",devprop.name);
+	devp.num_multiproc = devprop.multiProcessorCount;
 
-  	cudaMalloc(&devp.da,(params->M)*sizeof(float));
+	cudaMalloc(&devp.da,(params->M)*sizeof(float));
 	cudaMalloc(&devp.db,(params->M)*sizeof(float));
 	//k_init_paramsab<<<params->M/256,256>>>(devp.da,devp.db,3,params->p,params->q,params->M);
 	cudaMemcpy(devp.da,params->a,(params->M)*sizeof(float),cudaMemcpyHostToDevice);
@@ -100,11 +108,12 @@ void init_params(const char* file, int n, float delta, int debug, int algorithm,
 // initializes a vector dv with values val, from ini to end. At position ini, the value valini is given.
 __global__ void k_init_vec(float* dv, float val, float valini, uint ini, uint end) 
 {
-	uint i = threadIdx.x+blockDim.x*blockIdx.x;
-	if (i == ini)
-		dv[i] = valini;
-	if (i > ini && i < end)
-		dv[i] = val;
+	for(uint i = threadIdx.x+blockDim.x*blockIdx.x; i<end; i+=gridDim.x*blockDim.x) {
+		if (i == ini)
+			dv[i] = valini;
+		else if (i > ini)
+			dv[i] = val;
+	}
 }
 
 void init_vars(float x_init, float y_init, const RRT_PARAMS* params, RRT_VARS* vars)
@@ -126,8 +135,8 @@ void init_vars(float x_init, float y_init, const RRT_PARAMS* params, RRT_VARS* v
 	cudaMalloc(&devp.dy,(params->N)*sizeof(float));
 	//cudaMemcpy(devp.dx,vars->x,(params->N)*sizeof(float),cudaMemcpyHostToDevice);
 	//cudaMemcpy(devp.dy,vars->y,(params->N)*sizeof(float),cudaMemcpyHostToDevice);
-	k_init_vec<<<params->N/256,256>>>(devp.dx,3*params->p,x_init,0,params->N);
-	k_init_vec<<<params->N/256,256>>>(devp.dy,3*params->q,y_init,0,params->N);
+	k_init_vec<<<devp.num_multiproc*64,256>>>(devp.dx,3*params->p,x_init,0,params->N);
+	k_init_vec<<<devp.num_multiproc*64,256>>>(devp.dy,3*params->q,y_init,0,params->N);
 	
 	vars->px = (float*)malloc((params->N)*sizeof(float));
 	vars->py = (float*)malloc((params->N)*sizeof(float));
@@ -142,15 +151,10 @@ void init_vars(float x_init, float y_init, const RRT_PARAMS* params, RRT_VARS* v
 	//cudaMallocManaged(&vars->dp,(params->M)*sizeof(float));
 	cudaMalloc(&devp.ddp,(params->M)*sizeof(float));
 
-	size_t temp_storage_bytes1 = 0;
-	cub::DeviceReduce::Min(NULL, temp_storage_bytes1, devp.ddp, devp.dd, MAX(params->M,params->N));
-	cudaDeviceSynchronize();
-        size_t temp_storage_bytes2 = 0;
-	cudaMalloc(&devp.d_argmin,sizeof(KeyValuePair<int, int>));
-	cub::DeviceReduce::ArgMin(NULL, temp_storage_bytes2, devp.ddp, devp.d_argmin, MAX(params->M,params->N));
-	cudaDeviceSynchronize();
-	cudaMalloc(&devp.dcubtemp,MAX(temp_storage_bytes1,temp_storage_bytes2));
-	cudaMalloc(&devp.dm,sizeof(float));
+	cudaMalloc(&devp.d_argmin,sizeof(KeyValuePair<int, float>));
+	CubDebugExit(cub::DeviceReduce::ArgMin(NULL, devp.temp_bytes, devp.ddp, devp.d_argmin, MAX(params->M,params->N)));
+	CubDebugExit(cudaMalloc(&devp.dcubtemp,devp.temp_bytes));
+	CubDebugExit(cudaMalloc(&devp.dm,sizeof(float)));
 	
 	vars->x_rand = 0;
 	vars->y_rand = 0;
@@ -219,7 +223,7 @@ float rnd()
 
 
 // Squared distance from point (Cx,Cy) to segment [(Ax,Ay),(Bx,By)]
-__host__ __device__ float p_dist(float Cx, float Cy, float Ax, float Ay, float Bx, float By)
+float p_dist(float Cx, float Cy, float Ax, float Ay, float Bx, float By)
 {
 	float u = (Cx-Ax)*(Bx-Ax) + (Cy-Ay)*(By-Ay);
 	u /= (Bx-Ax)*(Bx-Ax) + (By-Ay)*(By-Ay);
@@ -234,26 +238,35 @@ __host__ __device__ float p_dist(float Cx, float Cy, float Ax, float Ay, float B
 	return (Px-Cx)*(Px-Cx) + (Py-Cy)*(Py-Cy);
 }
 
-__global__ void k_pdist(float* ddp, float* da, float*db, float x_nearest, float y_nearest, float x_new, float y_new, uint M) 
+__device__ inline float d_p_dist(float Cx, float Cy, float Ax, float Ay, float Bx, float By)
 {
-	uint i = threadIdx.x+blockDim.x*blockIdx.x;
-	if (i < M)
-		ddp[i] = p_dist(da[i],db[i],x_nearest,y_nearest,x_new,y_new);
+	float x = Bx-Ax, y = By-Ay;
+        float u = (Cx-Ax)*x + (Cy-Ay)*y;
+        u /= x*x + y*y;
+	u = __saturatef (u); // 0.0 <= u <= 1.0
+	float Px = Ax + u*x; // if u=0.0, Px= Ax, if u=1.0, Px = Bx, otherwise Px = Ax + u*(Bx-Ax)
+	float Py = Ay + u*y;
+	
+	return (Px-Cx)*(Px-Cx) + (Py-Cy)*(Py-Cy);
 }
 
+__global__ void k_pdist(float* ddp, float* da, float*db, float x_nearest, float y_nearest, float x_new, float y_new, uint M) 
+{
+	for(uint i = threadIdx.x+blockDim.x*blockIdx.x; i<M; i+=gridDim.x*blockDim.x)
+		ddp[i] = d_p_dist(da[i],db[i],x_nearest,y_nearest,x_new,y_new);
+}
 
 
 void obstacle_free(RRT_PARAMS* params, RRT_VARS* vars)
 {
 	// compute distances from all obstacles to segment [(x_nearest,y_nearest),(x_new,y_new)]
-	k_pdist<<<params->M/256,256>>>(devp.ddp, devp.da, devp.db, vars->x_nearest, vars->y_nearest, vars->x_new, vars->y_new, params->M);
-
+	k_pdist<<<MIN(params->M/256,devp.num_multiproc*8),256>>>(devp.ddp, devp.da, devp.db, vars->x_nearest, vars->y_nearest, vars->x_new, vars->y_new, params->M);
+	CubDebugExit(cudaDeviceSynchronize());
 	// Compute minimun distance	
-	size_t dummy;
-	cub::DeviceReduce::Min(devp.dcubtemp, dummy, devp.ddp, devp.dm, params->M);
+	CubDebugExit(cub::DeviceReduce::Min(devp.dcubtemp, devp.temp_bytes, devp.ddp, devp.dm, params->M));
 	cudaDeviceSynchronize();
 	float m;
-	cudaMemcpy(&m,devp.dm,sizeof(float),cudaMemcpyDeviceToHost);
+	CubDebugExit(cudaMemcpy(&m,devp.dm,sizeof(float),cudaMemcpyDeviceToHost));
 	// for some reason, the following line does not work
 	//float m = thrust::reduce(thrust::device, devp.ddp, devp.ddp + params->M, INF, thrust::minimum<float>());
 	/*float *m_pos = thrust::min_element(thrust::device, devp.ddp, devp.ddp + params->M);
@@ -294,18 +307,29 @@ XYD xyd_min2(XYD a, XYD b)
 #pragma omp declare reduction(xyd_min : XYD : omp_out=xyd_min2(omp_out,omp_in))\
 		initializer(omp_priv={0,0,INF})
 
+__global__ void k_sqdist(float* dd, float* dx, float*dy, float x_rand, float y_rand, uint index)
+{
+        for(uint i = threadIdx.x+blockDim.x*blockIdx.x; i<index; i+=gridDim.x*blockDim.x)
+		dd[i] = (dx[i] - x_rand) * (dx[i] - x_rand) + (dy[i] - y_rand) * (dy[i] - y_rand);
+}
 
 void nearest(RRT_PARAMS* params, RRT_VARS* vars)
 {
-	if (0) {   ///(vars->index > 500) {
+	if (vars->index > THRES_GPU) {
 		// compute squared distances from all points in RRT to (x_rand,y_rand)
-		/*struct d_squared_dist dsqdist_op;
-	        dsqdist_op.xr=vars->x_rand;
-		dsqdist_op.yr=vars->y_rand;
-		thrust::transform(thrust::device, devp.dx, devp.dx + vars->index, devp.dy, devp.dd, dsqdist_op);
-
+		k_sqdist<<<vars->index/256,256>>>(devp.dd, devp.dx, devp.dy, vars->x_rand, vars->y_rand, vars->index);
+		CubDebugExit(cudaDeviceSynchronize());
+		
+		cub::KeyValuePair<int, float> argmin;
 		// compute minimun distance and nearest point	
-		float *m_pos = thrust::min_element(thrust::device, devp.dd, devp.dd + vars->index);
+		CubDebugExit(cub::DeviceReduce::ArgMin(devp.dcubtemp, devp.temp_bytes, devp.dd, devp.d_argmin, vars->index));
+		cudaDeviceSynchronize();
+
+		CubDebugExit(cudaMemcpy(&argmin,devp.d_argmin,sizeof(cub::KeyValuePair<int,float>),cudaMemcpyDeviceToHost));
+		vars->d[0] = argmin.value;
+		vars->x_nearest = vars->x[argmin.key];
+		vars->y_nearest = vars->y[argmin.key];
+		/*float *m_pos = thrust::min_element(thrust::device, devp.dd, devp.dd + vars->index);
 		int pos = m_pos - devp.dd;//vars->d;
 		vars->x_nearest = vars->x[pos];
 		vars->y_nearest = vars->y[pos];
@@ -342,11 +366,11 @@ void extend_rrt(RRT_PARAMS* params, RRT_VARS* vars)
 	vars->px[vars->index] = vars->x_nearest;
 	vars->py[vars->index] = vars->y_nearest;
 
-	if (vars->index == 500) {
+	if (vars->index == THRES_GPU) {
 		cudaMemcpy(devp.dx,vars->x,(vars->index+1)*sizeof(float),cudaMemcpyHostToDevice);
 		cudaMemcpy(devp.dy,vars->y,(vars->index+1)*sizeof(float),cudaMemcpyHostToDevice);
 	}
-	else if (vars->index > 500) {
+	else if (vars->index > THRES_GPU) {
 		cudaMemcpy(devp.dx+vars->index,vars->x+vars->index,sizeof(float),cudaMemcpyHostToDevice);
 		cudaMemcpy(devp.dy+vars->index,vars->y+vars->index,sizeof(float),cudaMemcpyHostToDevice);
 	}
